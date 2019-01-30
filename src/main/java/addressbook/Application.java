026fd9e8806b75f51951b0d02b0cce0dea6d6355
Kshamina <kshamina.ghelani@carleton.ca>
2019-01-29 21:34:19 -0500

    lab 4

--- src/main/java/addressbook/Application.java
+++ src/main/java/addressbook/Application.java
@@ -14,47 +14,47 @@ public class Application {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    public CommandLineRunner demo(BuddyRepository repository) {
-        return (args) -> {
-            // save a couple of BuddyInfos
-            repository.save(new BuddyInfo("Jack", "613-111-1111"));
-            repository.save(new BuddyInfo("Chloe", "613-222-2222"));
-            repository.save(new BuddyInfo("Kim", "613-333-3333"));
-            repository.save(new BuddyInfo("David", "613-444-4444"));
-            repository.save(new BuddyInfo("Michelle", "613-555-5555"));
-
-            // fetch all BuddyInfos
-            log.info("BuddyInfos found with findAll():");
-            log.info("-------------------------------");
-            for (BuddyInfo BuddyInfo : repository.findAll()) {
-                log.info(BuddyInfo.toString());
-            }
-            log.info("");
-
-            // fetch an individual addressbook.BuddyInfo by ID
-            repository.findById(1L)
-                    .ifPresent(BuddyInfo -> {
-                        log.info("addressbook.BuddyInfo found with findById(1L):");
-                        log.info("--------------------------------");
-                        log.info(BuddyInfo.toString());
-                        log.info("");
-                    });
-
-            // fetch BuddyInfos by name
-            log.info("addressbook.BuddyInfo found with findByName('Bauer'):");
-            log.info("--------------------------------------------");
-            repository.findByName("Bauer").forEach(bauer -> {
-                log.info(bauer.toString());
-            });
-            // for (addressbook.BuddyInfo bauer : repository.findByLastName("Bauer")) {
-            // 	log.info(bauer.toString());
-            // }
-            log.info("");
-        };
-    }
+//    @Bean
+//    public CommandLineRunner demo(BuddyRepository repository) {
+//        return (args) -> {
+//            // save a couple of BuddyInfos
+//            repository.save(new BuddyInfo("Jack", "613-111-1111"));
+//            repository.save(new BuddyInfo("Chloe", "613-222-2222"));
+//            repository.save(new BuddyInfo("Kim", "613-333-3333"));
+//            repository.save(new BuddyInfo("David", "613-444-4444"));
+//            repository.save(new BuddyInfo("Michelle", "613-555-5555"));
+//
+//            // fetch all BuddyInfos
+//            log.info("BuddyInfos found with findAll():");
+//            log.info("-------------------------------");
+//            for (BuddyInfo BuddyInfo : repository.findAll()) {
+//                log.info(BuddyInfo.toString());
+//            }
+//            log.info("");
+//
+//            // fetch an individual addressbook.BuddyInfo by ID
+//            repository.findById(1L)
+//                    .ifPresent(BuddyInfo -> {
+//                        log.info("addressbook.BuddyInfo found with findById(1L):");
+//                        log.info("--------------------------------");
+//                        log.info(BuddyInfo.toString());
+//                        log.info("");
+//                    });
+//
+//            // fetch BuddyInfos by name
+//            log.info("addressbook.BuddyInfo found with findByName('Bauer'):");
+//            log.info("--------------------------------------------");
+//            repository.findByName("Bauer").forEach(bauer -> {
+//                log.info(bauer.toString());
+//            });
+//            // for (addressbook.BuddyInfo bauer : repository.findByLastName("Bauer")) {
+//            // 	log.info(bauer.toString());
+//            // }
+//            log.info("");
+//        };
+//    }
 
 }
--- src/main/java/addressbook/BuddyRepository.java
+++ src/main/java/addressbook/BuddyRepository.java
@@ -1,9 +1,12 @@
 package addressbook;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-public interface BuddyRepository extends CrudRepository<BuddyInfo, Long> {
-    List<BuddyInfo> findByName(String name);
+@RepositoryRestResource(collectionResourceRel = "buddies", path="buddies")
+public interface BuddyRepository extends PagingAndSortingRepository<BuddyInfo, Long> {
+    List<BuddyInfo> findByName(@Param("name") String name);
 }
