3750637c4163afb8a50a2173f6c7fddc61643ca7
Kshamina <kshamina.ghelani@carleton.ca>
2019-02-06 10:03:47 -0500

    lab 5 tests

---
+++ src/test/java/addressbook/ApplicationTest.java
@@ -0,0 +1,23 @@
+package addressbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTest {
+
+    @Autowired
+    private BuddyController controller;
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertNotNull(controller);
+    }
+
+}
